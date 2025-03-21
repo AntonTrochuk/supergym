21f5bcb39060cc88f9c6abfb0afcc24ed1b8ad63
Anton Trochuk <118171581+AntonTrochuk@users.noreply.github.com>
2025-03-21 23:08:10 +0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -8,3 +8,41 @@ import { priceTab } from './price-tab';
 
 playVideo();
 priceTab();
+
+import Swiper from 'swiper';
+import {Navigation} from 'swiper/modules';
+import 'swiper/css';
+
+const juriSwiper = document.querySelector('.swiper');
+const juriSlider = new Swiper(juriSwiper, {
+
+  modules: [Navigation],
+
+  loop: true,
+  speed: 500,
+  effect: 'fade',
+
+  breakpoints: {
+    1366: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 40,
+    },
+  },
+
+  navigation: {
+    nextEl: '.juri__button--next',
+    prevEl: '.juri__button--prev',
+  },
+});
+
+juriSlider.update();
